a59732c65c2706e4d17fd95afe7825c454cb121c
Raz Levi <razle102030@gmail.com>
2022-04-12 19:23:18 +0300

    Integrate Firebase
    #3

--- lib/global/utils.dart
+++ lib/global/utils.dart
@@ -2,15 +2,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wiki_places/global/types.dart';
-// import 'package:wiki_places/firebase_wrapper/google_analytics_repository.dart';
+import 'package:wiki_places/metrics/google_analytics.dart';
 
 // Navigation
 void navigateToPage(Widget page, AppPages? pageEnum) {
   Get.to(page);
-  // GoogleAnalytics.instance.logPageOpened(pageEnum);  // TODO
+  GoogleAnalytics.instance.logPageOpened(pageEnum);
 }
 
 void navigateBack(context) {
   Get.back();
   FocusScope.of(context).unfocus(); // Remove the keyboard
+}
+
+// Format
+extension En on Enum {
+  String toCleanString() => toString().split(".")[1];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,45 @@
 // ================= Main =================
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:wiki_places/localization/locale_controller.dart';
 import 'package:wiki_places/localization/resources/resources.dart';
 import 'package:wiki_places/pages/home.dart';
 
 void main() {
-  runApp(const WikiPlacesApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(WikiPlacesApp());
 }
 
 class WikiPlacesApp extends StatelessWidget {
-  const WikiPlacesApp({Key? key}) : super(key: key);
+  WikiPlacesApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'strAppName'.tr,
-      translations: Resources.instance,
-      locale: LocaleController.getCurrentLocale,
-      supportedLocales: LocaleController.getSupportedLocales(),
-      localeResolutionCallback: (deviceLocale, supportedLocale) => LocaleController.localeResolutionCallback(deviceLocale, supportedLocale),
-      fallbackLocale: LocaleController.getDefaultLocale,
-      localizationsDelegates: const [
-        GlobalCupertinoLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      home: HomePage(),
-    );
-  }
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(body: Center(child: Text(snapshot.error.toString())));
+          }
+
+          return (snapshot.connectionState != ConnectionState.done) ? const Center(child: CircularProgressIndicator()) : GetMaterialApp(
+                title: 'strAppName'.tr,
+                translations: Resources.instance,
+                locale: LocaleController.getCurrentLocale,
+                supportedLocales: LocaleController.getSupportedLocales(),
+                localeResolutionCallback: (deviceLocale, supportedLocale) => LocaleController.localeResolutionCallback(deviceLocale, supportedLocale),
+                fallbackLocale: LocaleController.getDefaultLocale,
+                localizationsDelegates: const [
+                  GlobalCupertinoLocalizations.delegate,
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                ],
+                home: HomePage(),
+              );
+        }
+      );
+    }
 }
