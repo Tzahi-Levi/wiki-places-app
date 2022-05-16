c03feea1f51c1fdad5ce39e10c988f5b9b245fdd
Raz Levi <razle102030@gmail.com>
2022-05-16 21:07:27 +0300

    tag and filters widgets

--- lib/widgets/search_place/filters.dart
+++ lib/widgets/search_place/filters.dart
@@ -4,8 +4,7 @@ import 'package:get/get.dart';
 import 'package:wiki_places/controllers/store_controller.dart';
 import 'package:wiki_places/global/constants.dart';
 import 'package:wiki_places/global/utils.dart';
-
-import '../tag.dart';
+import 'package:wiki_places/widgets/search_place/tag.dart';
 
 class Filters extends StatefulWidget {
   const Filters({Key? key}) : super(key: key);
@@ -18,6 +17,14 @@ class _FiltersState extends State<Filters> {
   final _storeController = Get.put(StoreController());
   final TextEditingController _filterController = TextEditingController();
 
+  List<Widget> get _getTags {
+    List<Widget> tags = [];
+    for (String filter in _storeController.placeFilters.value){
+      tags.add(Tag(title: filter, undoCallback: _addFilter));
+    }
+    return tags;
+  }
+
   @override
   void dispose() {
     _filterController.dispose();
@@ -27,54 +34,38 @@ class _FiltersState extends State<Filters> {
   void _addFilter([String? filter]) {
     filter ??= _filterController.text;
     if (filter.isNotEmpty) {
-      setState(() {
-        _storeController.addPlaceFilter(filter!);
-        _filterController.text = "";
-      });
+      _storeController.addPlaceFilter(filter!);
+      _filterController.text = "";
       FocusScope.of(Get.context!).unfocus(); // Remove the keyboard
     } else {
       displaySnackbar(content: 'strEmptyFilter'.tr, title: 'strError'.tr);
     }
   }
 
-  void _removeFilter(String filter) {
-    setState(() {
-      _storeController.removePlaceFilter(filter);
-    });
-    displayUndoSnackbar(content: 'strFilterRemoved'.tr, callback: () => _addFilter(filter));
-  }
-
-  List<Widget> createTags(){
-    List<Widget> tags = [];
-    for (String filter in _storeController.placeFilters.value){
-      tags.add(Tag(title: filter, onDeleted: ()=>_removeFilter(filter)));
-    }
-    return tags;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('strFilters'.tr),
-        Row(
+    return GetX<StoreController>(
+        builder: (store) => Column(
           children: [
-            Expanded(
-              child: TextField(
-                controller: _filterController,
-                decoration: InputDecoration(
-                  hintText: 'strAddFilter'.tr,
+            Text('strFilters'.tr),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _filterController,
+                    decoration: InputDecoration(
+                      hintText: 'strAddFilter'.tr,
+                    ),
+                  ),
                 ),
-              ),
+                IconButton(onPressed: _addFilter, icon: const Icon(GlobalConstants.addIcon)),
+              ],
             ),
-            IconButton(onPressed: _addFilter,
-                icon: const Icon(GlobalConstants.addIcon)),
+            Wrap(
+              children: _getTags,
+            ),
           ],
         ),
-        Wrap(
-          children: createTags(),
-        ),
-      ],
     );
   }
 }
